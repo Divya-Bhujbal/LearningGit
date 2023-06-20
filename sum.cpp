9ecbe6d64385aeaf5d16a2fa620290336e118877
Divya Shailendra Bhujbal <divyabhujbal2018@gmail.com>
2023-06-20 09:50:52 +0530

    sum.cpp includes new variable

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,11 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int a=3;
-    int b=5;
-    int add=a+b;
-    cout<<"sum is : "<<add<<endl;
+int main()
+{
+    int a = 3;
+    int b = 5;
+    int c = 9;
+    int add = a + b + c;
+    cout << "sum is : " << add << endl;
     return 0;
 }
