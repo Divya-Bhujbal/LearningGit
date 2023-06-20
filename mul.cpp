874373223ca0c568b87ca80334d3579ac4891b45
Divya Shailendra Bhujbal <divyabhujbal2018@gmail.com>
2023-06-20 10:53:51 +0530

    added multiplication code

--- mul.cpp
+++ mul.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int a=9;
+    int b=6;
+    int mul=a*b;
+    cout<<"multiplication is : "<<mul<<endl;
+    return 0;
+    //changes made
     
 }
